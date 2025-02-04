62fb3ad737f83273d88a13267c4eb00895958dbb
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-02-04 12:01:38 +0530

    saitharun

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// import React from "react";
+
+
+// // function App() {
+// //   // let ele = React.createElement("div",{className:"APP"},
+// //   //   React.createElement("h1",null,"Header")
+// //   // )
+// //   let Username="saitharun";
+// //   return (
+// //     // <div className="App">
+// //     //   <h1 style={{color:"red"}}>mission Accomplished !!</h1>
+// //     // </div>
+// //     // ele
+
+// //     <div>
+// //       <h1>{Username}</h1>
+// //       <p>
+// //         {100+120}
+// //       </p>
+// //     </div>
+//   );
+// }
+
+// export default App;
+
+//  components
+//CBC
+// import React from "react";
+// class App extends React.Component{
+//   render(){
+//     return(
+//     <h1>Class Bsed Component</h1>
+//     )
+//   }
+// }
+// export default App;
+
+//
