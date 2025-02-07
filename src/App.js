dd4ef17cdb697b370c398e19bcb2a9efb3fe6672
saitharun2005 <2211cs010572@mallareddyuniversity.ac.in>
2025-02-07 12:59:19 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,120 +1,143 @@
-// // // import React from "react";
+// // // // import React from "react";
 
 
-// // // // function App() {
-// // // //   // let ele = React.createElement("div",{className:"APP"},
-// // // //   //   React.createElement("h1",null,"Header")
-// // // //   // )
-// // // //   let Username="saitharun";
-// // // //   return (
-// // // //     // <div className="App">
-// // // //     //   <h1 style={{color:"red"}}>mission Accomplished !!</h1>
-// // // //     // </div>
-// // // //     // ele
+// // // // // function App() {
+// // // // //   // let ele = React.createElement("div",{className:"APP"},
+// // // // //   //   React.createElement("h1",null,"Header")
+// // // // //   // )
+// // // // //   let Username="saitharun";
+// // // // //   return (
+// // // // //     // <div className="App">
+// // // // //     //   <h1 style={{color:"red"}}>mission Accomplished !!</h1>
+// // // // //     // </div>
+// // // // //     // ele
 
-// // // //     <div>
-// // // //       <h1>{Username}</h1>
-// // // //       <p>
-// // // //         {100+120}
-// // // //       </p>
-// // // //     </div>
-// // //   );
+// // // // //     <div>
+// // // // //       <h1>{Username}</h1>
+// // // // //       <p>
+// // // // //         {100+120}
+// // // // //       </p>
+// // // // //     </div>
+// // // //   );
+// // // // }
+
+// // // // export default App;
+
+// // // //  components
+// // // //CBC
+// // // // import React from "react";
+// // // // class App extends React.Component{
+// // // //   render(){
+// // // //     return(
+// // // //     <h1>Class Bsed Component</h1>
+// // // //     )
+// // // //   }
+// // // // }
+// // // // export default App;
+
+// // // //component composition
+// // // import React from 'react'
+// // // import Navbar from './components/Navbar'
+// // // import Main from './components/Main'
+// // // import Sidebar1 from './components/Sidebar1'
+// // // import Sidebar2 from './components/Sidebar2'
+// // // import Footer from './components/Footer'
+
+
+// // // const App = () => {
+// // //   return (
+// // //     <div className='app'>
+// // //         <Navbar/>
+// // //         <Main/>
+    
+// // //     <div className='sidebar'>
+// // //         <Sidebar1/>
+// // //         <Sidebar2/>
+// // //     </div>
+// // //         <Footer/>
+// // //     </div>
+    
+// // //   )
 // // // }
 
-// // // export default App;
+// // // export default App
+// // import React, {Component} from "react";
+// // import CBCPropEx from "./propsex.jsx/CBCPropEx";
+// // import FBCPropEx from  "./propsex.jsx/FBCPropEx";
 
-// // //  components
-// // //CBC
-// // // import React from "react";
-// // // class App extends React.Component{
-// // //   render(){
-// // //     return(
-// // //     <h1>Class Bsed Component</h1>
-// // //     )
-// // //   }
+
+
+// // export default class App extends Component {
+// //   render() {
+// //     console.log(this);
+// //         return (
+// //       <div>
+// //         {/* <CBCPropEx
+// //         username="tharun"
+// //         age={20}
+// //         hobbies={["playing","Gaming","Reading books"]}
+// //         address={{city :"Hyderabad", area: "dulapally"}}
+// //         sendFun={function(){alert("hi i am tharun")}}
+// //         /> */}
+
+// //         <FBCPropEx
+// //         username="tharun"
+// //         isMarried={true}
+// //         hobbies={["playing","Gaming","Reading books"]}
+// //         />
+
+// //       </div>
+// //     )
+// //   }
 // // // }
-// // // export default App;
-
-// // //component composition
 // // import React from 'react'
-// // import Navbar from './components/Navbar'
-// // import Main from './components/Main'
-// // import Sidebar1 from './components/Sidebar1'
-// // import Sidebar2 from './components/Sidebar2'
-// // import Footer from './components/Footer'
-
-
-// // const App = () => {
+// // import PropChildEx from './propsex/PropChildEx'
+// // import SubChildEx from './propsex/SubChildEx'
+// // export const App = () => {
 // //   return (
-// //     <div className='app'>
-// //         <Navbar/>
-// //         <Main/>
-    
-// //     <div className='sidebar'>
-// //         <Sidebar1/>
-// //         <Sidebar2/>
+// //     <div>
+// //       <PropChildEx num={1000}>
+// //         <h1>This is data coming from props children</h1>
+// //       <SubChildEx/>
+// //       </PropChildEx>
 // //     </div>
-// //         <Footer/>
-// //     </div>
-    
 // //   )
 // // }
 
 // // export default App
-// import React, {Component} from "react";
-// import CBCPropEx from "./propsex.jsx/CBCPropEx";
-// import FBCPropEx from  "./propsex.jsx/FBCPropEx";
-
-
-
-// export default class App extends Component {
-//   render() {
-//     console.log(this);
-//         return (
-//       <div>
-//         {/* <CBCPropEx
-//         username="tharun"
-//         age={20}
-//         hobbies={["playing","Gaming","Reading books"]}
-//         address={{city :"Hyderabad", area: "dulapally"}}
-//         sendFun={function(){alert("hi i am tharun")}}
-//         /> */}
-
-//         <FBCPropEx
-//         username="tharun"
-//         isMarried={true}
-//         hobbies={["playing","Gaming","Reading books"]}
-//         />
-
-//       </div>
-//     )
-//   }
-// // }
 // import React from 'react'
-// import PropChildEx from './propsex/PropChildEx'
-// import SubChildEx from './propsex/SubChildEx'
-// export const App = () => {
+// import FBCStateEx from './stateexample/FBCStateEx'
+// // import CBCStateEx from './stateexample/CBCStateEx'
+
+
+// const App = () => {
 //   return (
 //     <div>
-//       <PropChildEx num={1000}>
-//         <h1>This is data coming from props children</h1>
-//       <SubChildEx/>
-//       </PropChildEx>
+//       {/* <CBCStateEx/> */}
+//       <FBCStateEx/>
 //     </div>
 //   )
 // }
 
 // export default App
 import React from 'react'
-import FBCStateEx from './stateexample/FBCStateEx'
-// import CBCStateEx from './stateexample/CBCStateEx'
-
-
+// import CBCStateex from './stateEx/CBCStateex'
+// // import FBCStateex from './stateEx/FBCStateex'
+// import FunctionalComponent from './components/FunctionalComponent'
+// // import "./components/dd/Styles.css"
+// import "./Styles.css"
+import FunctionalComponent from './components/FunctionalComponents'
+import "./components/Styles.css"
+import styles from './components/myStyle.module.css'
 const App = () => {
   return (
     <div>
-      {/* <CBCStateEx/> */}
-      <FBCStateEx/>
+      {/* <CBCStateex/>
+       */}
+       {/* <FBCStateex/> */}
+       <h1 class="heading">This is on styling class</h1>
+       <h1 class={styles.error}>Error</h1>
+       <FunctionalComponent clgName="MRU" apply={true}/>
     </div>
   )
 }
--- src/stateexample/FBCStateEx.jsx
+++ src/stateexample/FBCStateEx.jsx
@@ -6,10 +6,14 @@ const FBCStateEx = () => {
     const changevalue=()=>{
         setValue(1000);
     }
+    const increment=()=>{
+        setValue(value+1)
+    }
   return (
     <div>FBCStateEx
         <h1>{value}</h1>
         <button onClick={changevalue}>ChangeValue</button>
+        <button onClick={increment}>Login</button>
     </div>
   )
 }
